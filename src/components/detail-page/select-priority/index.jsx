d05b31ecf7e548754b182730a8949edf1c9a6cba
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-04-16 17:34:53 +0800

    create select priority input

---
+++ src/components/detail-page/select-priority/index.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef, useState } from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "../../../assets";
+
+export default function SelectPriority({ options, defaultValue, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedLabel, setSelectedLabel] = useState("Pilih priority");
+  const [selectedColor, setSelectedColor] = useState("");
+
+  const selectRef = useRef(null);
+
+  useEffect(() => {
+    if (defaultValue) {
+      const selectedOption = options.find((opt) => opt.value === defaultValue);
+      setSelectedLabel(selectedOption.label);
+      setSelectedColor(selectedOption.color);
+    }
+  }, [defaultValue]);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (selectRef.current && !selectRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [selectRef]);
+
+  function toggleDropdown() {
+    setIsOpen(!isOpen);
+  }
+
+  function handleOptionClick({ value, label, color }) {
+    onSelect(value);
+    setSelectedLabel(label);
+    setSelectedColor(color);
+    setIsOpen(false);
+  }
+
+  return (
+    <div className="select-priority" ref={selectRef}>
+      <div
+        className="select-priority__label"
+        style={{ backgroundColor: isOpen ? "#E5E5E5" : "#FFFFFF" }}
+        onClick={toggleDropdown}
+      >
+        <div>
+          {selectedColor && (
+            <div className="circle" style={{ backgroundColor: selectedColor }} />
+          )}
+          {selectedLabel}
+        </div>
+        <img src={isOpen ? ChevronUpIcon : ChevronDownIcon} alt="chevron" />
+      </div>
+      {isOpen && (
+        <ul className="select-priority__options">
+          {options.map((option) => (
+            <li key={option.value} onClick={() => handleOptionClick(option)}>
+              <div className="circle" style={{ backgroundColor: option.color }} />{" "}
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
