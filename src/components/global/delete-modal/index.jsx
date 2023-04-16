12c2701e9b8bd10c14032c84175815f5289a014f
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-04-16 17:33:54 +0800

    create delete modal

---
+++ src/components/global/delete-modal/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Modal from "../modal";
+import { ModalDeleteIcon } from "../../../assets";
+import Button from "../button";
+
+export default function DeleteModal({
+  isOpen,
+  onClose,
+  onDelete,
+  highlightText,
+}) {
+  return (
+    <Modal isOpen={isOpen}>
+      <div className="delete-modal">
+        <img src={ModalDeleteIcon} alt="modal-delete-icon" />
+        <p>
+          Apakah anda yakin menghapus activity{" "}
+          <span className="text-bold">{highlightText}</span>
+        </p>
+        <div className="delete-modal__footer">
+          <Button
+            textColor="#4A4A4A"
+            backgroundColor="#F4F4F4"
+            backgroundColorHover="#d9d9d9"
+            onClick={onClose}
+          >
+            Batal
+          </Button>
+          <Button
+            backgroundColor="#ED4C5C"
+            backgroundColorHover="#a43944"
+            onClick={onDelete}
+          >
+            Hapus
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
