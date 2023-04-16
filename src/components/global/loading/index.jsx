e69a03bdba2d9f43ec8d7669de299fbe8265c619
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-04-16 17:33:43 +0800

    create loading

---
+++ src/components/global/loading/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div className="loading__animation" />
+      <h1 className="loading__title">Memuat data ...</h1>
+    </div>
+  );
+}
