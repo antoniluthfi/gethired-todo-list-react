275f9af068c04121ffe3c536eb437fda23790ce5
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-04-16 17:35:38 +0800

    create context

---
+++ src/AppContext.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useEffect, useState } from "react";
+
+export const AppContext = createContext(null);
+
+export default function AppProvider({ children }) {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showAddTodoModal, setShowAddTodoModal] = useState(false);
+  const [alertConfig, setAlertConfig] = useState({
+    show: false,
+    message: "",
+  });
+
+  useEffect(() => {
+    if (alertConfig.show) {
+      setTimeout(() => {
+        setAlertConfig({
+          show: false,
+          message: "",
+        });
+      }, 2500);
+    }
+  }, [alertConfig.show]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        showDeleteModal,
+        setShowDeleteModal,
+        alertConfig,
+        setAlertConfig,
+        showAddTodoModal,
+        setShowAddTodoModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
